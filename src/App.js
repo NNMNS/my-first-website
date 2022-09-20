38f91c749064ff539b5f8783cd1a662a247eb169
nan <manassa.p10@gmail.com>
2022-09-20 14:05:01 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
+import { ChakraProvider } from '@chakra-ui/react'
 import './App.css';
-
+//import { Button } from '@chakra-ui/react'
+import { Button, ButtonGroup,Spinner, Input } from '@chakra-ui/react'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+        <Button colorScheme="green" size='xs'>Button</Button>
+        <Button colorScheme='red' size='sm'>Red</Button>
+        <Button colorScheme='yellow' size='md'>Yellow</Button>
+      
+        <Spinner size='xs' color='blue.500' />
+        <Spinner size='sm' color='red.500'/>
+        <Spinner size='md' color='green.500'/>
+
+        <Input variant='outline' placeholder='extra small size' size='xs' />
+        <Input variant='filled' placeholder='small size' size='sm' />
+        <Input variant='flushed' placeholder='medium size' size='md' />
+      </ChakraProvider>
+      
   );
 }
 
